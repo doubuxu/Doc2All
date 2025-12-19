150893853ef5907c4efc22e7690f2ee54f51bb4c
Huangyc <1738112976@qq.com>
2025-12-19 15:33:09 +0800

    使用ppt_generate函数运行baselineModel1

--- fullPipeline/pipeline.py
+++ fullPipeline/pipeline.py
@@ -9,6 +9,8 @@ from jinja2 import Template
 import json
 from generateCaption import getCaption,postProcessImages
 from utils.JsonTools import load_json,save_json
+from htmlGenerate import htmlCodeWithbase64
+from utils.htmlTools import load_html,save_html
 #pdf_path=""
 #parse_doc()
 
@@ -16,29 +18,59 @@ def args_analyse():
     parser = argparse.ArgumentParser(description='input args')
     parser.add_argument('--doc_path', help='input Document path')
     parser.add_argument('-o', '--output', default='output', help='output path')
-    #parser.add_argument('--overwrite', action='store_true', help='¸²¸ÇÒÑÓĞÎÄ¼ş')
+    #parser.add_argument('--overwrite', action='store_true', help='ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ä¼ï¿½')
     args = parser.parse_args()
     return args
 
 
+def ppt_generate(input_path,output_path):
+    input_path=Path(arg.doc_path).resolve()
+    output_path=Path(arg.output).resolve()
+    file_name=input_path.stem
+    parse_doc(input_path,output_path)
+
+    postProcessImages(output_path,file_name)
+
+    md_path=Path(output_path)/file_name/f'{file_name}.md'
+    image_dict_path=Path(output_path)/file_name/"dict"/"images.json"
+    table_dict_path=Path(output_path)/file_name/"dict"/"tables.json"
+
+    #content_planï¿½×¶ï¿½
+    content_plan_prompt_template=Template(open('prompts/content_plan.txt').read())
+    markdown_content=Path(md_path).read_text(encoding='utf-8')
+    images_dict=load_json(image_dict_path)
+    table_dict=load_json(table_dict_path)
+    content_plan_prompt=content_plan_prompt_template.render(document=markdown_content,images=images_dict,tables=table_dict)
+    content_plan_json=Path(output_path)/file_name/f"{file_name}_content_plan.json"
+    contentPlan=content_plan(content_plan_prompt,content_plan_json)
+
+    #generateï¿½×¶ï¿½
+    html_generate_prompt_template=Template(open('prompts/htmlGenerate.txt').read())
+    html_generate_prompt=html_generate_prompt_template.render(contentplan=contentPlan)
+    html_code=htmlCodeWithbase64(html_generate_prompt,output_path,file_name)
+    html_path=Path(output_path)/file_name/f"{file_name}.html"
+    save_html(html_path,html_code)
+
 if __name__=="__main__":
 
-    #ÊäÈë²ÎÊıÖ¸¶¨output£¬ÔÚoutputÏÂ½á¹¹´æ´¢mdµÈÎÄ¼ş
+    #è¾“å…¥å‚æ•°æŒ‡å®šoutputï¼Œåœ¨outputä¸‹ç»“æ„å­˜å‚¨mdç­‰æ–‡ä»¶
     arg=args_analyse()
     input_path=Path(arg.doc_path).resolve()
     output_path=Path(arg.output).resolve()
     file_name=input_path.stem
-    #»ùÓÚmineru×öpdfÎÄµµ½âÎö£¬½âÎö½á¹û±£´æµ½output_path
+    ppt_generate(input_path,output_path)
+    """
+    #åŸºäºmineruåšpdfæ–‡æ¡£è§£æï¼Œè§£æç»“æœä¿å­˜åˆ°output_path
     parse_doc(input_path,output_path)
 
-    #content plan½×¶Î
-    #»ñÈ¡mdÎÄ¼şµÄÂ·¾¶
+    #content planï¿½×¶ï¿½
+    #ï¿½ï¿½È¡mdï¿½Ä¼ï¿½ï¿½ï¿½Â·ï¿½ï¿½
     md_path=Path(output_path)/file_name/f'{file_name}.md'
     image_dict_path=Path(output_path)/file_name/"dict"/"images.json"
     table_dict_path=Path(output_path)/file_name/"dict"/"tables.json"
     #print(md_path)
     
-    """
+    
     with open(image_dict_path,'r',encoding='utf-8') as f:
         image_dict=json.load(f)
     for index,img in enumerate(image_dict):
@@ -47,20 +79,19 @@ if __name__=="__main__":
 
     with open(image_dict_path,'w',encoding='utf-8') as f:
         json.dump(image_dict,f,ensure_ascii=False,indent=2)        
-    """
+    
     postProcessImages(output_path,file_name)
 
 
-    #¼ÓÔØprompt
-    #prompt_template=Template(open('prompts/paper_content_plan.txt').read())
     prompt_template=Template(open('prompts/content_plan.txt').read())
     markdown_content=Path(md_path).read_text(encoding='utf-8')
     images_dict=load_json(image_dict_path)
     table_dict=load_json(table_dict_path)
     prompt=prompt_template.render(document=markdown_content,images=images_dict,tables=table_dict)
     content_plan_json=Path(output_path)/file_name/f"{file_name}_content_plan.json"
-    content_plan(prompt,content_plan_json)
-    
+    contentPlan=content_plan(prompt,content_plan_json)
+    """
+
     
 
 
